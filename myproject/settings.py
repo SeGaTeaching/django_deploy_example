f960de2269d5879d26bd659c0191a22e077ef57a
Sebastian Gaus <Sebastian.Gaus@enders-training.de>
2024-12-05 12:32:02 +0100

    added dropdown menu in navigation

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,8 +31,8 @@ load_dotenv(env_path)
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['segateach.pythonanywhere.com', '127.0.0.1', 'segateach.*']
 
